713aac6c601ca43dc58773d59ddce1e594b6b130
Tyler Harding <tharding732@gmail.com>
2018-05-18 09:13:16 -0700

    Don't care

--- ProjectViewer/src/ProjectViewer/Main.java
+++ ProjectViewer/src/ProjectViewer/Main.java
@@ -1,20 +1,19 @@
+package ProjectViewer;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-package ProjectViewer;
-
-public class ProjectViewer extends Application{
+public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Better Project Viewer");
-        primaryStage.setScene(new Scene(root, 920, 400));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        //testing github
     }
 
 
