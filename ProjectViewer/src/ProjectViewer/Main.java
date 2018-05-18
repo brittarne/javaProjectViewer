a7e44cbb77c1e660558b69b61306ba7e324bc52b
Tyler Harding <tharding732@gmail.com>
2018-05-18 10:22:29 -0700

    yes

--- ProjectViewer/src/ProjectViewer/Main.java
+++ ProjectViewer/src/ProjectViewer/Main.java
@@ -15,6 +15,8 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 920, 400));
         primaryStage.show();
 
+
+        //final test to put comment into master
     }
 
 
